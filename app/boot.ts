3eefa1a0c5d2b2ab6731b5021b1bc12f38e5f10d
ggodreau <godreau@gmail.com>
2017-05-07 22:42:38 +0100

    ul li functional base exercise

--- app/boot.ts
+++ app/boot.ts
@@ -1,5 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
+//import {AppComponent} from './app.component'
+import {ExerciseComponent} from './exercise.component'
 import {ROUTER_PROVIDERS} from 'angular2/router'
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+//bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+bootstrap(ExerciseComponent, [ROUTER_PROVIDERS]);
