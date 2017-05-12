9fa36cb124e2d8dc69e32e1cfa5d5fac4e19d1d5
ggodreau <godreau@gmail.com>
2017-05-12 19:01:44 +0100

    final working prototype

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,6 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AlbumsComponent} from './albums.component';
 import {AlbumComponent} from './album.component';
 import {ContactComponent} from './contact.component';
-//import {RouterOutlet} from 'angular2/router';
-//import {RouterLink} from 'angular2/router';
 
 @RouteConfig([
 	{
