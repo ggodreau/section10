900a2c33d439b2c465580abb92460b6cdb3acc6a
ggodreau <godreau@gmail.com>
2017-05-15 19:39:34 +0100

    cleaned up boot.ts comments

--- app/boot.ts
+++ app/boot.ts
@@ -1,7 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
-//import {AppComponent} from './app.component'
 import {ExerciseComponent} from './exercise.component'
 import {ROUTER_PROVIDERS} from 'angular2/router'
 
-//bootstrap(AppComponent, [ROUTER_PROVIDERS]);
 bootstrap(ExerciseComponent, [ROUTER_PROVIDERS]);
